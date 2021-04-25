fd004cb213b2acbe840095c229f7f7563921702d
semeka13 <semeka13@yandex.ru>
2021-04-25 13:59:58 +1000

    add_product added

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
+from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib import messages  # import messages
@@ -46,14 +47,31 @@ def sign_in(request):
     return render(request, 'store/login.html', {'form': LoginForm(), "error": error})
 
 
-@login_required
 def add_product(request):
-    if request.POST:
-        form = AddProductForm(request.POST)
-        if form.is_valid():
-            product = form.save()
-            return redirect('buy')
-        messages.error(request, "Unsuccessful registration. Invalid information.")
+    if request.POST and request.FILES["file"]:
+
+        condition = request.POST.get("condition")
+        brand = request.POST.get("brand")
+        year = request.POST.get("year")
+        model = request.POST.get("model")
+        size = request.POST.get("size")
+        extra_info = request.POST.get("extra_info")
+        price = request.POST.get("price")
+        # file = request.POST.get("file")
+        user = request.user
+        product = Products(condition=condition, brand=brand,
+                           season=year, model=model, size=size,
+                           price=price, extra_info=extra_info,
+                           seller=user)
+        product.save()
+        file = request.FILES["file"]
+        fs = FileSystemStorage(location='./static/product_pictures')
+        filename = fs.save(file.name, file)
+        image = Image(product=product, picture=file)
+        image.save()
+
+        return redirect('buy')
+    messages.error(request, "Unsuccessful registration. Invalid information.")
     return render(request, 'store/sell.html', {'form': AddProductForm(), 'error': "error_message"})
 
 
